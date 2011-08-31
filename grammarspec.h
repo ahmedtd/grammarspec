084cca46be43d1c124db698afb09d36cde4b622e
Taahir Ahmed <ahmed.taahir@gmail.com>
2011-08-31 12:29:04 -0500

    still working

--- grammarspec.h
+++ grammarspec.h
@@ -5,8 +5,9 @@
 #ifndef GRAMMARSPEC
 #define GRAMMARSPEC
 
+#include <set>
+#include <sstream>
 #include <string>
-using std::string;
 
 // WARNING: when defining a grammar tree, it must all be in the same scope
 // Otherwise, the pointer links will break
@@ -18,15 +19,14 @@ namespace grammarspec
 class element
 {
 public:
-	const virtual result* match(const string &text,
-	                            int match_start_index) const = 0;
+	virtual result* match(const std::istream &parse_stream) const = 0;
 };
 
 //Abstract base class for parse results
 class result
 {
 public:
-	result(const string *in_original)
+	result(const std::string *in_original)
 	:
 		original(in_original)
 	{
@@ -38,36 +38,36 @@ public:
 		delete original;
 	}
 
-	const virtual string result_type() const = 0;
+	const virtual std::string result_type() const = 0;
 	const virtual void * result() const = 0;
 
 	//Get the original substring corresponding to this result
-	const virtual string* original()
+	const virtual std::string* original()
 	{
 		return original;
 	}
 private:
 
 	//The original substring corresponding to this result
-	const string *original;
+	const std::string *original;
 };
 
 class result_token : public result
 {
 public:
-	result_token(const string *original)
+	result_token(const std::string *original)
 	:
 		result(original)
 	{
 		
 	}
 
-	const virtual string result_type() const
+	const virtual std::string result_type() const
 	{
-		return string("string:token");
+		return std::string("string:token");
 	}
 
-	const virtual string* result() const
+	const virtual std::string* result() const
 	{
 		return original;
 	}	
@@ -76,19 +76,19 @@ public:
 class result_vector : public result
 {
 public:
-	result_vector(const string *original)
+	result_vector(const std::string *original)
 	:
 		result(original)
 	{
 		
 	}
 
-	const virtual string result_type() const
+	const virtual std::string result_type() const
 	{
-		return string("vector:result");
+		return std::string("vector:result");
 	}
 
-	const virtual vector<result *> *result() const
+	const virtual vector<result *>* result() const
 	{
 		
 	}
@@ -106,7 +106,9 @@ public:
 		
 	}
 
-	int match(const string &text, int match_start_index) const
+	// TODO: update to properly return result *
+	virtual int result_vector* match(const std::string &text,
+	                                 int match_start_index) const
 	{
 		int arg1_match_index = arg1->match(text, match_start_index);
 		if(arg1_match_index == match_start_index)
@@ -134,7 +136,9 @@ public:
 		
 	}
 
-	int match(const string &text, int match_start_index) const
+	// TODO: update to properly return result*
+	virtual int result_vector* match(const std::string &text,
+	                                 int match_start_index) const
 	{
 		int match_new_index = 0;
 		do
@@ -153,31 +157,103 @@ private:
 	const element *element_to_match;
 };
 
-class stringliteral : public element
+//match up to n characters from chars_to_match
+//if n==-1, match any number of chars
+class charset : public element
 {
 public:
-	stringliteral(const string &in_literal_to_match)
+	charset(const std::set<char> &in_chars_to_match, int in_n = -1)
 	:
-		literal_to_match(in_literal_to_match)
+		chars_to_match(in_chars_to_match),
+		n(in_n)
+	{
+		
+	}
+
+	virtual result_token* match(const std::istream &parse_stream) const
+	{
+		std::sstream matched_stream;
+
+		bool continue_matching = (chars_to_match != 0);
+		while(continue_matching)
+		{
+			if(n > 0)
+			{
+				n--;
+				if(n==0)
+					continue_matching = false;
+			}
+
+			char c = parse_stream.peek();
+			if(chars_to_match.find(c) != set<char>::end)
+			{
+				parse_stream.get();
+				matched_stream.put(c);
+			}
+			else
+				break;
+		}
+
+		if(matched_stream.str().length() != 0)
+			return new result_token(new string(matched_stream.str()));
+		else
+			return NULL;
+	}
+private:
+	std::set<char> chars_to_match;
+	int n;
+};
+
+class whitespace : public charset
+{
+public:
+	whitespace()
+	:
+		charset(" \t\n")
+	{
+		
+	}	
+};
+
+class literal : public element
+{
+public:
+	stringliteral(const std::string &in_literal_to_match,
+	              bool in_whitespace_allowed = true)
+	:
+		literal_to_match(in_literal_to_match),
+		whitespace_allowed(in_whitespace_allowed)
 	{
 		
 	}
 
 	//Returns the new match_start_index.  If this is not changed, then there
 	//was no match
-	virtual result_token* match(const string &text,
-	                            int match_start_index) const
+	virtual result_token* match(const std::istream &parse_stream) const
 	{
-		int index_of_literal = text.find(literal_to_match, match_start_index);
+		if(whitespace_allowed)
+		{
+			whitespace wmatch;
+			result *wmatch_result = wmatch.match(parse_stream);
+
+			if(result != NULL)
+				delete result; //We own the memory pertaining to results
+		}
+
+		// I need to nondestructively get the next literal_to_match.length()
+		// characters from the stream;
+
+		// get() them, then use putback()
 
 		if(index_of_literal == match_start_index)
-			return new result_token(original);
+			return new result_token(text);
 		else
 			return NULL;
 	}
 
 private:
-	string literal_to_match;
+	std::string literal_to_match;
+	bool   whitespace_allowed;
 };
 
 }//end namespace grammarspec
